8fa96c74d1935a816c3a4adaa2f9226aefe07530
Michael Felix <michael.felix@calbaptist.edu>
2019-03-04 15:26:23 -0800

    Updated

--- src/pizzasf/PizzaStore.java
+++ src/pizzasf/PizzaStore.java
@@ -2,5 +2,19 @@ package pizzasf;
 
 //IMPLEMENT ME
 public class PizzaStore {
+    private final SimplePizzaFactory factory;
 
+    public PizzaStore() {
+        this.factory = new SimplePizzaFactory();
+
+        public Pizza orderPizza (String type){
+            Pizza pizza = factory.createPizza(type);
+
+            pizza.prepare();
+            pizza.bake();
+            pizza.cut();
+            pizza.box();
+            return pizza;
+        }
+    }
 }
